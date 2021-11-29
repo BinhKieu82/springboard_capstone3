93aae166dd35aa6869688e0456c606759263131a
brucekieu <brucekieu@gmail.com>
2021-11-29 23:22:32 +0100

    02 data wrangling & EDA

--- functions.py
+++ functions.py
@@ -1,5 +1,37 @@
 import numpy as np
 import pandas as pd
+# from zipline.assets._assets import Equity  # Required for USEquityPricing
+# from zipline.pipeline.data import USEquityPricing
+# from zipline.pipeline.classifiers import Classifier
+# from zipline.pipeline.engine import SimplePipelineEngine
+# from zipline.pipeline.loaders import USEquityPricingLoader
+# from zipline.utils.numpy_utils import int64_dtype
+
+
+def get_high_lows_lookback(high, low, lookback_days):
+    """
+    Get the highs and lows in a lookback window.
+    
+    Parameters
+    ----------
+    high : DataFrame
+        High price for each ticker and date
+    low : DataFrame
+        Low price for each ticker and date
+    lookback_days : int
+        The number of days to look back
+    
+    Returns
+    -------
+    lookback_high : DataFrame
+        Lookback high price for each ticker and date
+    lookback_low : DataFrame
+        Lookback low price for each ticker and date
+    """
+    #TODO: Implement function
+    lookback_high = high.shift(1).rolling(lookback_days).max()
+    lookback_low = low.shift(1).rolling(lookback_days).min()
+    return lookback_high, lookback_low
 
 def bucketing_factors(factor,quantiles):
     """
@@ -7,3 +39,34 @@ def bucketing_factors(factor,quantiles):
     """
     buckets = pd.qcut(factor, q=quantiles, labels=False)
     return buckets
+
+def resample_prices(prices, freq='M'):
+    """
+    Resample close prices for each ticker at specified frequency.
+    """
+    return prices.resample(freq).last()
+
+
+def compute_log_returns(prices):
+    """
+    Compute log returns for each ticker.
+    """
+    log_prices = np.log(prices)
+    return log_prices - log_prices.shift(1)
+
+def shift_returns(returns, shift_n):
+    """
+    Generate shifted returns
+   """
+    return returns.shift(shift_n)
+
+
+def build_pipeline_engine(bundle_data, trading_calendar):
+    pricing_loader = PricingLoader(bundle_data)
+
+    engine = SimplePipelineEngine(
+        get_loader=pricing_loader.get_loader,
+        calendar=trading_calendar.all_sessions,
+        asset_finder=bundle_data.asset_finder)
+
+    return engine
--- visualizations.py
+++ visualizations.py
@@ -1,4 +1,73 @@
 import matplotlib.pyplot as plt
+import scipy.stats
+# from colour import Color
+import numpy as np
+import pandas as pd
+import plotly as py
+import plotly.graph_objs as go
+# go.renderers.default = "vscode"
+import plotly.offline as pyo
+pyo.init_notebook_mode(connected=True)
+
+
+def generate_config():
+    return {'showLink': False, 'displayModeBar': False, 'showAxisRangeEntryBoxes': True}
+
+
+def plot_high_low(prices, lookback_high, lookback_low, title):
+    config = generate_config()
+    layout = go.Layout(title=title)
+
+    stock_trace = go.Scatter(
+        name='Index',
+        x=prices.index,
+        y=prices,
+        line={'color': 'red'})
+    high_trace = go.Scatter(
+        x=lookback_high.index,
+        y=lookback_high,
+        name='Column lookback_high',
+        fill = None,
+        line={'color':'#2D3ECF'})
+    low_trace = go.Scatter(
+        x=lookback_low.index,
+        y=lookback_low,
+        name='Column lookback_low',
+        fill='tonexty',
+        fillcolor='rgba(0,250,0,0.4)',
+        line={'color': '#B6B2CF'})
+
+    pyo.iplot({'data': [stock_trace, high_trace, low_trace], 'layout': layout}, config=config)
+
+# def plot_bolinger(prices,title):
+#     fig = go.Figure()
+#     layout = go.Layout(title=title)
+
+#     fig.add_traces(go.Scatter(
+#         name='close',
+#         x=prices.index,
+#         y=prices.close,
+#         line={'color': 'black'}))
+#     fig.add_traces(go.Scatter(
+#         name='sma_band',
+#         x=prices.index,
+#         y=prices.sma,
+#         line={'color': 'red'}))
+#     fig.add_traces(go.Scatter(
+#         x=prices.index,
+#         y=prices.upper,
+#         name='high_band',
+#         fill=None,
+#         line={'color':'#2D3ECF'}))
+#     fig.add_traces(go.Scatter(
+#         x=prices.index,
+#         y=prices.lower,
+#         name='low_band',
+#         fill='tonexty',
+#         fillcolor='rgba(0,250,0,0.4)',
+#         line={'color': '#B6B2CF'}))
+
+#     pyo.iplot({'data': fig, 'layout': layout})
 
 def plot_bar(data,factor):
     """
